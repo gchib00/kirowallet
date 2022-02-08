1475aed3f02162f773b8ae88f1a991570e110ee8
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2022-02-08 21:56:36 +0100

    configured eslint, added basic global styling, initialized Navbar styling

---
+++ components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+interface Props {
+  children: JSX.Element | JSX.Element[];
+}
+
+export const Layout = ({ children }: Props) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
+};
